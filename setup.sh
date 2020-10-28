874f26a8741b1daf0f986f8ad464c19f22107622
Flavio Carrara De Capua <flavio@carrara.us>
2020-10-28 13:35:15 -0300

    switched to vm-driver=virtualbox (so the errors did not occur)

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 minikube version
-minikube start --cpus 4 --memory 8192 --v=7
+minikube start --cpus 4 --memory 8192 --v=7 --vm-driver=virtualbox
 kubectl version
 minikube status
 echo "Check this out:\n minikube ip: \t\t- `minikube ip` \n 01_metallb.yaml: `cat srcs/01_metallb.yaml | tail -1` \n"
@@ -9,7 +9,6 @@ eval $(minikube docker-env)
 minikube addons enable metallb
 kubectl apply -f srcs/01_metallb.yaml
 docker build -t nginx:services srcs/02_nginx_context/
-#cd srcs/02_nginx_context && docker build -t nginx:services . && cd ../..
 kubectl apply -f srcs/02_nginx.yaml
 
 
@@ -21,4 +20,4 @@ kubectl apply -f srcs/02_nginx.yaml
 echo '\n42 São Paulo :: ft_services :: fde-capu\n'
 sleep 1
 minikube dashboard &
-#echo 'use: `minikube dashboard`\n'
+echo '\n42 São Paulo :: ft_services :: fde-capu\n'
