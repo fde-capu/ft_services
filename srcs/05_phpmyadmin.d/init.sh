7752275d0c1ba34dc7124184f3374f03ce3e659b
Flavio Carrara De Capua <flavio@carrara.us>
2020-11-13 15:30:51 -0300

    Welcome to phpMyAdmin

--- srcs/05_phpmyadmin.d/init.sh
+++ srcs/05_phpmyadmin.d/init.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+
+var_username="user42"
+var_userdomain="localhost"
+randomBlowfishSecret=$(openssl rand -base64 32)
+
+mysql -e "GRANT SELECT, INSERT, UPDATE, DELETE ON phpmyadmin.* TO '$var_username'@'$var_userdomain'"
+mysql -e "FLUSH PRIVILEGES"
+sed -i -e "s|cfg\['blowfish_secret'\] = ''|cfg['blowfish_secret'] = '$randomBlowfishSecret'|" /var/www/webapps/phpmyadmin/config.inc.php
+
 php-fpm7
 nginx -g "daemon off;" && tail -f /dev/null
-
-#var_username="user42"
-#var_userdomain="localhost"
-#randomBlowfishSecret=$(openssl rand -base64 32)
-
-#mysql -e "GRANT SELECT, INSERT, UPDATE, DELETE ON phpmyadmin.* TO '$var_username'@'$var_userdomain'"
-#mysql -e "FLUSH PRIVILEGES"
-#sed -e "s|cfg\['blowfish_secret'\] = ''|cfg['blowfish_secret'] = '$randomBlowfishSecret'|" /var/www/ft_server/html/phpmyadmin/config.sample.inc.php > /var/www/ft_server/html/phpmyadmin/config.inc.php
