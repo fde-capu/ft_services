b54df6af2818670274312702d45e39f1f97fe489
Flavio Carrara De Capua <flavio@carrara.us>
2020-11-25 08:14:52 -0300

    new error message milestone

--- srcs/06_ftps.d/init.sh
+++ srcs/06_ftps.d/init.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-echo "This is a test file." > /test.txt
-vsftpd
+echo "This is a test file for ftps located at root folder /." > /test.txt
+echo "This is a test file for ftps located on /var/lib/ftp." > /var/lib/ftp/test.txt
+vsftpd &
 tail -f /dev/null
