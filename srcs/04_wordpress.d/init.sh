0b963e6afa5b22a76dfbc8b1ddd01233f0ced3c9
Flavio Carrara De Capua <flavio@carrara.us>
2020-12-11 16:42:56 -0300

    adjustment of file locations

--- srcs/04_wordpress.d/init.sh
+++ srcs/04_wordpress.d/init.sh
@@ -9,13 +9,13 @@ php7-bz2 php7-iconv php7-pdo_dblib php7-curl php7-ctype php7-common \
 php7-xml php7-imap php7-cgi fcgi php7-pdo php7-pdo_mysql php7-posix \
 php7-ldap php7-dom php7-session
 apk add nginx openssl wget
-
 mkdir -p /run/nginx
 mkdir -p /etc/nginx/ssl
 rm -f /etc/nginx/conf.d/default.conf
 mv /wp_nginx.conf /etc/nginx/conf.d
 wget http://wordpress.org/latest.tar.gz
 tar -zxvf latest.tar.gz -C /var/www
+mv /wp-config.php /var/www/wordpress
 php-fpm7 &
 nginx &
 tail -f /dev/null
