df973b748740d84b149afc2f428113b57ae7e2ba
fde-capu <flavio@carrara.us>
2020-12-23 19:50:46 +0100

    telegraf auto host name

--- srcs/07_grafana.d/init.sh
+++ srcs/07_grafana.d/init.sh
@@ -5,7 +5,7 @@ tar -zxvf grafana-*.linux-amd64.tar.gz --strip 1
 mkdir -p /var/lib/grafana/dashboards
 ln -s /influxdb-datasource.yml /conf/provisioning/datasources/influxdb-datasource.yml
 ln -s /grafana-dashboard-provider.yml /conf/provisioning/dashboards/grafana-dashboard-provider.yml
-ln -s /ft_services-dashboard.json /var/lib/grafana/dashboards/ft_services-dashboard.json
+ln -s /ft_services-*.json /var/lib/grafana/dashboards/
 grafana-server --config /grafana.ini &
 /bin/sh /telegraf.sh
 tail -f /dev/null
--- srcs/07_grafana.d/telegraf.sh
+++ srcs/07_grafana.d/telegraf.sh
@@ -5,4 +5,6 @@ tar -C /usr/src -xzf telegraf-*.tar.gz
 rm -f /etc/telegraf/telegraf.conf
 mkdir /etc/telegraf/telegraf.d
 cp -a /usr/src/telegraf*/usr/bin/telegraf /usr/bin/
+HN=${HOSTNAME%%-*}
+sed -i s/{HOSTNAME}/$HN/ /telegraf.conf
 exec telegraf -config /telegraf.conf &
