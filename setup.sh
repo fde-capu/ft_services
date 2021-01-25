af23f90b8c344072e12a0293055bd6a6192acebf
Flavio Carrara De Capua <flavio@carrara.us>
2021-01-25 17:22:12 -0300

    x

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ export CHANGE_MINIKUBE_NONE_USER=true
 
 echo "\n\nminikube ignite\n===========\n"
 sudo -E minikube start --v=7 --vm-driver=$DRIVER
+sudo minikube kubectl -- get pods -A
 mkip=`minikube ip`
 
 echo "\n\nBuild: 03_mysql\n===========\n"
