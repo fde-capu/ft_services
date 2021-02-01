c886f3b5961dbc50a3d502e41bba12085d69210b
fde-capu <flavio@carrara.us>
2021-02-01 18:49:05 +0100

    wordpress.sql dynamic external ip

--- setup.sh
+++ setup.sh
@@ -12,12 +12,10 @@ sudo -E minikube start --v=7 --vm-driver=$DRIVER
 sudo usermod -aG docker user42
 sudo chown user42:docker /run/docker.sock
 ls -l /run/docker.sock
-sleep 10
+sleep 20
 sudo minikube kubectl -- get pods -A
 mkip=`minikube ip`
 
-echo "\n\nBuild: 03_mysql\n===========\n"
-docker build -t mysql:service srcs/03_mysql.d
 echo "\n\nBuild: 04_wordpress\n===========\n"
 docker build -t wordpress:service srcs/04_wordpress.d
 echo "\n\nBuild: 05_phpmyadmin\n===========\n"
@@ -33,7 +31,23 @@ echo "\n\nkubectl applies\n===========\n"
 sudo usermod -aG docker user42
 sudo chown user42:docker /run/docker.sock
 
+echo "\n(enable metallb)\n"
+sudo -E minikube addons enable metallb
+kubectl apply -f srcs/01_metallb.yaml
+
 kubectl apply -f srcs/01.5_vols.yaml
+kubectl apply -f srcs/04_wordpress.yaml
+waitip="<pending>"
+while [ "$waitip" = "<pending>" ]; do
+	echo -n "."
+	waitip=$(kubectl get svc | grep wordpress | awk '{printf "%s", $4}')
+	sleep 3
+done
+sed "s/{WORDPRESS_EXTERNAL_IP}/$waitip/g" srcs/02_nginx.d/nginx.conf-template > srcs/02_nginx.d/nginx.conf
+sed "s/{WORDPRESS_EXTERNAL_IP}/$waitip/g" srcs/03_mysql.d/wordpress.sql-template > srcs/03_mysql.d/wordpress.sql
+
+echo "\n\nBuild: 03_mysql\n===========\n"
+docker build -t mysql:service srcs/03_mysql.d
 kubectl apply -f srcs/03_mysql.yaml
 waitip="<pending>"
 while [ "$waitip" = "<pending>" ]; do
@@ -41,24 +55,11 @@ while [ "$waitip" = "<pending>" ]; do
 	waitip=$(kubectl get svc | grep mysql | awk '{printf "%s", $3}')
 	sleep 2
 done
-echo "\n(enable metallb)\n"
-sudo -E minikube addons enable metallb
-kubectl apply -f srcs/01_metallb.yaml
-kubectl apply -f srcs/04_wordpress.yaml
 kubectl apply -f srcs/05_phpmyadmin.yaml
 kubectl apply -f srcs/06_ftps.yaml
 kubectl apply -f srcs/08_influxdb.yaml
 kubectl apply -f srcs/07_grafana.yaml
 
-echo "\n\nBuild and apply: 02_nginx\n===========\n"
-waitip="<pending>"
-while [ "$waitip" = "<pending>" ]; do
-	echo -n "."
-	waitip=$(kubectl get svc | grep wordpress | awk '{printf "%s", $4}')
-	sleep 3
-done
-
-sed "s/{WORDPRESS_EXTERNAL_IP}/$waitip/g" srcs/02_nginx.d/nginx.conf-template > srcs/02_nginx.d/nginx.conf
 docker build -t nginx:service srcs/02_nginx.d
 kubectl apply -f srcs/02_nginx.yaml
 
